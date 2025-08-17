b22888adb0c146b95be489893d4782b7ae3a06b2
malik <malik@localhost.localdomain>
2025-08-17 02:00:01 -0400

    second

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,7 +1,8 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.failureConditions
+import jetbrains.buildServer.configs.kotlin.v2019_2.failureConditions.BuildFailureOnText
+import jetbrains.buildServer.configs.kotlin.v2019_2.failureConditions.failOnText
 
 version = "2019.2"
 
@@ -41,6 +42,13 @@ object BuildAndTest : BuildType({
 
     triggers {
         vcs {
+        }
+    }
+
+    failureConditions {
+        failOnText {
+            conditionType = BuildFailureOnText.ConditionType.CONTAINS
+            pattern = "FAILED"
         }
     }
 })
